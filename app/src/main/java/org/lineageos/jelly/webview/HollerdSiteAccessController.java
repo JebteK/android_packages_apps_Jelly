9a261b8e37b80ca1176e24a7124e790f3b7bbb48
Andrew Jebasingh <andrew@hollerd.com>
2020-05-11 14:45:33 -0500

    Trying to get the imei instead of the androidId which seems to keep changing.

--- app/src/main/java/org/lineageos/jelly/webview/HollerdSiteAccessController.java
+++ app/src/main/java/org/lineageos/jelly/webview/HollerdSiteAccessController.java
@@ -1,11 +1,13 @@
 package org.lineageos.jelly.webview;
 
 //import android.util.Log;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.provider.Settings.Secure;
 import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -23,21 +25,38 @@ import java.net.URL;
  */
 public class HollerdSiteAccessController {
 
+    private static final String TAG = "[Jelly]";
+
     private Context mContext;
 
     private String mAndroidId;
+
+    private String mImei;
+
+    private static int HOLLERD_SLOT_INDEX = 0;
 
     public HollerdSiteAccessController(Context context) {
 
         //set the context
         mContext = context;
 
+        Log.v(TAG, "Initializing...");
+
         initialize();
     }
 
+    @SuppressLint("MissingPermission")
     public void initialize() {
         //load the androidId
         mAndroidId = Secure.getString(mContext.getContentResolver(), Secure.ANDROID_ID);
+
+        Log.v(TAG, "androidId: " + mAndroidId);
+
+        TelephonyManager telephonyManager = (TelephonyManager) mContext.getSystemService(mContext.TELEPHONY_SERVICE);
+
+        mImei = telephonyManager.getImei(HOLLERD_SLOT_INDEX);
+
+        Log.v(TAG, "Imei: " + mImei);
     }
 
     public String getRequestUrl(String urlToCheck) {
@@ -49,7 +68,7 @@ public class HollerdSiteAccessController {
             if (urlToCheck.contains("hollerd.com"))
                 return true;
 
-            URL url = new URL("https://api.hollerd.com/Hos/CheckSiteAccess/?id=" + mAndroidId + "&url=" + urlToCheck);
+            URL url = new URL("https://api.hollerd.com/Hos/CheckSiteAccess/?id=" + mAndroidId + "&url  =" + urlToCheck);
 
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("POST");
@@ -67,27 +86,27 @@ public class HollerdSiteAccessController {
                 String responseLine = null;
                 while ((responseLine = br.readLine()) != null) {
                     response.append(responseLine.trim());
-                    //Log.i("getWhitelistNumbers", "responseLine: " + responseLine, this);
+                    Log.v(TAG, "responseLine: " + responseLine);
                 }
 
                 //and return it
                 return response.toString().contains("True");
             }
             catch (Exception exception) {
-                //Log.e("getWhitelistNumbers", exception, "Process Response", this);
+                Log.e(TAG, "Process Response", exception);
             }
         }
         catch (MalformedURLException malformedURLException) {
-            //Log.e("getWhitelistNumbers", malformedURLException, "Bad URL?", this);
+            Log.e(TAG, "Bad URL?", malformedURLException);
         }
         catch (UnsupportedEncodingException unsupportedEncodingException) {
-            //Log.e("getWhitelistNumbers", unsupportedEncodingException, "Bad Encoding?", this);
+            Log.e(TAG, "Bad Encoding?", unsupportedEncodingException);
         }
         catch (IOException ioException) {
-            //Log.e("getWhitelistNumbers", ioException, "Bad IO?", this);
+            Log.e(TAG, "Bad IO?", ioException);
         }
         catch (Exception e) {
-            //Log.e("getWhitelistNumbers", e, "Bad???", this);
+            Log.e(TAG, "Bad???", e);
         }
         finally {
             //Log.endSession();
